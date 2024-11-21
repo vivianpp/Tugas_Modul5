a8e2f3e7fd5337085e9057692072a0837b0538ab
vivianpp <220711795@students.uajy.ac.id>
2024-11-21 19:07:03 +0700

    Update Tugas_Modul5_A_Vivi_11795.py

--- Tugas_Modul5_A_Vivi_11795.py
+++ Tugas_Modul5_A_Vivi_11795.py
@@ -4,7 +4,7 @@ import numpy as np
 from PIL import Image
 import os
 
-model_path = r'C:\Users\ASUS\Documents\Tugas5_A_11795\best_model_tf.h5'
+model_path = r'best_model_tf.h5'
 
 if os.path.exists(model_path):
     try:
